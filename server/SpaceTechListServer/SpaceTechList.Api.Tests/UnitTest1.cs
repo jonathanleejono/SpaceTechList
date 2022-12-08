9dac23f262527466f0a122706156bb6580e788b5
Jonathan <Jonathan@iMac.ht.home>
2022-12-07 21:02:42 -0500

    moving files and renaming solution

---
+++ server/SpaceTechListServer/SpaceTechList.Api.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace SpaceTechList.Api.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
